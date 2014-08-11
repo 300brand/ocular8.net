3de0f7eb1ba64eec0b0d80f8fa240d821fa6151b
jake <bigjake2s@gmail.com>
2014-08-11 13:18:36 -0400

    rpi-spider-data: Added LOCK TABLES grant for spider user

--- rpi-spider-data/mysql_setup.sh
+++ rpi-spider-data/mysql_setup.sh
@@ -10,7 +10,7 @@ while [[ RET -ne 0 ]]; do
 done
 
 /usr/bin/mysql -uroot -e "CREATE USER 'spider'@'%' IDENTIFIED BY PASSWORD '*5C2901F96BBBA397912EF7416EF4BC9B3B2B7672'"
-/usr/bin/mysql -uroot -e "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE ON spider.* TO 'spider'@'%'"
+/usr/bin/mysql -uroot -e "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, LOCK TABLES ON spider.* TO 'spider'@'%'"
 /usr/bin/mysql -uroot -e "CREATE USER 'admin'@'%' IDENTIFIED BY PASSWORD '*95211EBE089EAED2B3C998E7F0DA9DA4F3E21627'"
 /usr/bin/mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION"
 /usr/bin/mysql -uroot -e "CREATE DATABASE IF NOT EXISTS spider"
