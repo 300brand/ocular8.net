b559edb4f08f55e69929f051e9b5387ecc796953
jake <bigjake2s@gmail.com>
2014-08-11 13:10:23 -0400

    rpi-spider-data: Updated automatic permission script

--- rpi-spider-data/mysql_setup.sh
+++ rpi-spider-data/mysql_setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-/usr/bin/mysqld_safe > /dev/null 2>&1 &
+# /usr/bin/mysqld_safe > /dev/null 2>&1 &
 
 RET=1
 while [[ RET -ne 0 ]]; do
 	echo "=> Waiting for confirmation of MySQL service startup"
 	sleep 5
-	mysql -uroot -e "status" > /dev/null 2>&1
+	/usr/bin/mysql -uroot -e "status" > /dev/null 2>&1
 	RET=$?
 done
 
@@ -14,4 +14,3 @@ done
 /usr/bin/mysql -uroot -e "CREATE USER 'admin'@'%' IDENTIFIED BY PASSWORD '*95211EBE089EAED2B3C998E7F0DA9DA4F3E21627'"
 /usr/bin/mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION"
 /usr/bin/mysql -uroot -e "CREATE DATABASE IF NOT EXISTS spider"
-/usr/bin/mysqladmin -uroot shutdown
