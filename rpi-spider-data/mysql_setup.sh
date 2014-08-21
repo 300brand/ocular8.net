e2822fe102f201dd03a684fce434dcfe0ad9a12a
jake <bigjake2s@gmail.com>
2014-08-21 11:15:08 -0400

    Updates to mysql_setup script

--- rpi-spider-data/mysql_setup.sh
+++ rpi-spider-data/mysql_setup.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 # /usr/bin/mysqld_safe > /dev/null 2>&1 &
 
+shopt -s nullglob
+DIR=/var/lib/mysql
+if [[ `echo ${DIR}/*` -z ]]; then
+	echo "New installation: chowning, then installing to ${DIR}"
+	chown mysql:mysql $DIR
+	mysql_install_db --user=mysql
+fi
+
 RET=1
 while [[ RET -ne 0 ]]; do
 	echo "=> Waiting for confirmation of MySQL service startup"
@@ -9,8 +17,10 @@ while [[ RET -ne 0 ]]; do
 	RET=$?
 done
 
+echo "MySQL up, setting up users"
 /usr/bin/mysql -uroot -e "CREATE USER 'spider'@'%' IDENTIFIED BY PASSWORD '*5C2901F96BBBA397912EF7416EF4BC9B3B2B7672'"
 /usr/bin/mysql -uroot -e "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, LOCK TABLES ON spider.* TO 'spider'@'%'"
 /usr/bin/mysql -uroot -e "CREATE USER 'admin'@'%' IDENTIFIED BY PASSWORD '*95211EBE089EAED2B3C998E7F0DA9DA4F3E21627'"
 /usr/bin/mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION"
 /usr/bin/mysql -uroot -e "CREATE DATABASE IF NOT EXISTS spider"
+echo "Done"
